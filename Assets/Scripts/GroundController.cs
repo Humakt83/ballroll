6c134a0825171487c9989800fc1a76b7d4ec7d03
Humakt83 <urlact@yahoo.com>
2017-08-14 19:53:43 +0300

    Initial commit with playable game

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+    private readonly float TILT = 30.0f;
+    private readonly float MINIMUM_AXIS_TILT = -30.0f;
+
+    private void FixedUpdate() {
+        float tiltAroundZ = Input.GetAxis("Horizontal") * TILT;
+        float tiltAroundX = Input.GetAxis("Vertical") * TILT;
+        Quaternion target = Quaternion.Euler(tiltAroundX * -1, 0, tiltAroundZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 0.2f);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text scoreText;
+    public Text winText;
+    //private Rigidbody rigidBody;
+    private int scoreCount;    
+
+    private void Start() {
+        //rigidBody = GetComponent<Rigidbody>();
+        scoreCount = 0;
+        SetScoreText();
+        winText.text = "";
+    }
+
+    /**private void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidBody.AddForce(movement * speed);
+    }**/
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pickup")) {
+            other.gameObject.SetActive(false);
+            scoreCount++;
+            SetScoreText();
+        }
+    }
+
+    private void SetScoreText() { 
+        scoreText.text = "Score: " + scoreCount;
+        if (scoreCount >= 12) {
+            winText.text = "Victory";
+        }
+    }
+}
