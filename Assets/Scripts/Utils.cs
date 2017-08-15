d0bb3c22860b25ed4df107441000fb0ab493d2d0
Humakt83 <urlact@yahoo.com>
2017-08-15 22:17:58 +0300

    Quit button

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+    
+    public void Exit() {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
